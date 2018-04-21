1f61625112ada013c5a703f5cb94a0a79d19c067
Kacper Kania <kacper1095@gmail.com>
2018-04-22 01:18:40 +0200

    Playground

--- src/furnishing/testing_playground.py
+++ src/furnishing/testing_playground.py
@@ -1,9 +1,10 @@
-from src.furnishing.furniture import EllipseFurniture, RoundedFurniture, RectangularFurniture
+from src.furnishing.furniture_collection import Window
+from src.furnishing.furniture_construction import EllipseFurniture, RectangularFurniture
 from src.furnishing.room import Room, RoomDrawer
 
-
 room = Room(15, 15)
-room.add_furniture(RectangularFurniture(7, 3, 0, 3, 3))
-room.add_furniture(EllipseFurniture(7, 7, 0, 3, 1))
+room.add_furniture(RectangularFurniture(7, 3, 90, 3, 3, False))
+room.add_furniture(EllipseFurniture(7, 7, 0, 3, 1, False))
+room.add_furniture(Window(15, 7, room.width, room.height, 4))
 drawer = RoomDrawer(room)
 drawer.draw_all()
