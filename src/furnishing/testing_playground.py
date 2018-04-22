13925b01183928e0a0b7efa17a5123e9532c6d59
Kacper Kania <kacper1095@gmail.com>
2018-04-22 19:46:17 +0200

    Added new furniture

--- src/furnishing/testing_playground.py
+++ src/furnishing/testing_playground.py
@@ -1,11 +1,16 @@
 from src.furnishing.furniture_collection import *
-from src.furnishing.furniture_construction import EllipseFurniture, RectangularFurniture
+from src.furnishing.furniture_construction import *
 from src.furnishing.room import Room, RoomDrawer
 
 room = Room(15, 15)
-room.add_furniture(RectangularFurniture(7, 3, 90, 3, 3, False))
-room.add_furniture(EllipseFurniture(7, 7, 0, 3, 1, False))
+room.add_furniture(RectangularFurniture(7, 3, 90, 3, 3, False, True))
+room.add_furniture(Cupboard(7, 7, 45, 3, 2, False))
+
+room.add_furniture(RoundedCornersFurniture(7, 13, 35, 1, 1, True, True))
 room.add_furniture(Window(15, 7, room.width, room.height, 4))
 room.add_furniture(Door(0, 7, room.width, room.height, 4))
+room.add_furniture(Tv(13, 7, 270, 5, 3, 3, 1, True, name='tv'))
 drawer = RoomDrawer(room)
-drawer.draw_all()
+drawer.draw_all(
+    tv_tv='yellow'
+)
