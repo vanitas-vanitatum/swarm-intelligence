b02e94366ea51090eff8d6c45a2233f05db0e876
Kacper Kania <kacper1095@gmail.com>
2018-04-22 19:45:04 +0200

    Caclualting angle between vectors

--- src/furnishing/math_utils.py
+++ src/furnishing/math_utils.py
@@ -27,3 +27,17 @@ class MatrixUtils:
         new_rotation = normalized.dot(rotation_matrix)
         restored = new_rotation + rotate_point
         return restored
+
+    @staticmethod
+    def get_angle_between_vectors(vector_1, vector_2):
+        """
+        Standard math equation.
+        :param vector_1: Vector in form [x, y].
+        :param vector_2: Vector in form [x, y].
+        :return: Angle in degrees between vectors.
+        """
+        length_1 = np.linalg.norm(vector_1, ord=2)
+        length_2 = np.linalg.norm(vector_2, ord=2)
+        cos_angle = vector_1.dot(vector_2.T) / (length_1 * length_2)
+        radian_angle = np.arccos(cos_angle)
+        return np.rad2deg(radian_angle)
