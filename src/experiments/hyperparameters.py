5ac39f24b4e0b34c7425de5b04c687c32f5498b6
Kacper <kacper1095@gmail.com>
2018-05-05 15:09:33 +0200

    Added sample experiment

--- src/experiments/hyperparameters.py
+++ src/experiments/hyperparameters.py
@@ -7,7 +7,7 @@ import pandas as pd
 from src.callbacks import ParamStatsStoreCallback
 from src.constraints import LessThanConstraint, MoreThanConstraint
 from src.experiments.utils import get_experiment_dir
-from src.stop_conditions import StepsNumberStopCondition
+from src.stop_conditions import EarlyStoppingCondition
 from src.swarm_algorithms import *
 from src.test_functions import Ackley
 
@@ -34,28 +34,84 @@ WHALE_PARAMS = {
 TESTED_FUNCTION = Ackley()
 STEPS_NUMBER = 300
 POPULATION_SIZE = 50
+RUN_TIMES = 10
 
 
-def test_single(algorithm_constructor, boundaries, params, default_params):
+def test_single(algorithm_constructor, constraints, params):
     for key, all_values in params.items():
-        logger = pd.DataFrame(columns=['epoch', 'best', 'avg', 'worst', 'param value'])
+        logger = pd.DataFrame(columns=['dimension2_mean', 'dimension2_std', 'dimension10_mean', 'dimension10_std',
+                                       'param_value', 'param_name'])
         for value in all_values:
-            alg = algorithm_constructor(**default_params)
-            setattr(alg, key, value)
-            alg.compile(TESTED_FUNCTION.fitness_function, boundaries)
+            best_dim_2, best_dim_10 = [], []
+            for _ in range(RUN_TIMES):
+                default_params, boundaries = get_params_and_boundaries(2, constraints, algorithm_constructor)
+                alg = algorithm_constructor(**default_params)
+                setattr(alg, key, value)
+                alg.compile(TESTED_FUNCTION.fitness_function, boundaries)
 
-            stats_callback = ParamStatsStoreCallback()
-            alg.go_swarm_go(StepsNumberStopCondition(STEPS_NUMBER), [stats_callback])
-            epoch, best, worst, avg = stats_callback.get_params()
-            logger = logger.append({'epoch': epoch, 'best': best, 'avg': avg, 'worst': worst, 'param name': key,
-                                    'param value': value},
+                stats_callback = ParamStatsStoreCallback()
+                alg.go_swarm_go(EarlyStoppingCondition(30), [stats_callback])
+                epoch, best, worst, avg = stats_callback.get_params()
+                best_dim_2.append(epoch * best)
+
+                default_params, boundaries = get_params_and_boundaries(10, constraints, algorithm_constructor)
+                alg = algorithm_constructor(**default_params)
+                setattr(alg, key, value)
+                alg.compile(TESTED_FUNCTION.fitness_function, boundaries)
+
+                stats_callback = ParamStatsStoreCallback()
+                alg.go_swarm_go(EarlyStoppingCondition(30), [stats_callback])
+                epoch, best, worst, avg = stats_callback.get_params()
+                best_dim_10.append(epoch * best)
+
+            logger = logger.append({'dimension2_mean': np.mean(best_dim_2),
+                                    'dimension2_std': np.std(best_dim_2),
+                                    'dimension10_mean': np.mean(best_dim_10),
+                                    'dimension10_std': np.std(best_dim_10),
+                                    'param_name': key,
+                                    'param_value': value},
                                    ignore_index=True)
         logger = logger.round(4)
         yield logger, key
 
 
+def get_params_and_boundaries(dimension, constraints, alg):
+    boundaries = np.zeros((dimension, 2))
+    boundaries[:, 0] = -32.768
+    boundaries[:, 1] = 32.768
+    if alg == ParticleSwarmOptimisation:
+        default_params = dict(
+            population_size=POPULATION_SIZE,
+            nb_features=dimension,
+            constraints=constraints,
+            inertia=1.,
+            divergence=1.,
+            learning_factor_1=2.,
+            learning_factor_2=2.,
+            seed=None
+        )
+    elif alg == WhaleAlgorithm:
+        rho, eta = WhaleAlgorithm.get_optimal_eta_and_rho_zero(boundaries)
+        default_params = dict(
+            population_size=POPULATION_SIZE,
+            nb_features=dimension,
+            constraints=constraints,
+            attenuation_of_medium=eta,
+            intensity_at_source=rho,
+            seed=None
+        )
+    else:
+        default_params = dict(
+            population_size=POPULATION_SIZE,
+            nb_features=dimension,
+            constraints=constraints,
+            delta_potential_length_parameter=1,
+            seed=None
+        )
+    return default_params, boundaries
+
+
 def test():
-    d = [2, 10]
     names = ['PSO', 'Whale', 'QSO']
     algs_params = OrderedDict({
         ParticleSwarmOptimisation: PSO_PARAMS,
@@ -66,44 +122,11 @@ def test():
     constraints = MoreThanConstraint(-32.768).und(LessThanConstraint(32.768))
     for name, (alg, params) in zip(names, algs_params.items()):
         print(f'\r{alg}', end='')
-        for dimension in d:
-            boundaries = np.zeros((dimension, 2))
-            boundaries[:, 0] = -32.768
-            boundaries[:, 1] = 32.768
-            if alg == ParticleSwarmOptimisation:
-                default_params = dict(
-                    population_size=POPULATION_SIZE,
-                    nb_features=dimension,
-                    constraints=constraints,
-                    inertia=1.,
-                    divergence=1.,
-                    learning_factor_1=2.,
-                    learning_factor_2=2.,
-                    seed=None
-                )
-            elif alg == WhaleAlgorithm:
-                rho, eta = WhaleAlgorithm.get_optimal_eta_and_rho_zero(boundaries)
-                default_params = dict(
-                    population_size=POPULATION_SIZE,
-                    nb_features=dimension,
-                    constraints=constraints,
-                    attenuation_of_medium=eta,
-                    intensity_at_source=rho,
-                    seed=None
-                )
-            else:
-                default_params = dict(
-                    population_size=POPULATION_SIZE,
-                    nb_features=dimension,
-                    constraints=constraints,
-                    delta_potential_length_parameter=1,
-                    seed=None
-                )
-            for logger, param_name in test_single(alg, boundaries, params, default_params):
-                logger.to_csv(os.path.join(csv_dir, f'{name}-{param_name}-{dimension}.csv'), index=False,
-                              float_format='%.4f')
-                logger.to_latex(os.path.join(latex_dir, f'{name}-{param_name}-{dimension}.csv'), index=False,
-                                float_format='%.4f')
+        for logger, param_name in test_single(alg, constraints, params):
+            logger.to_csv(os.path.join(csv_dir, f'{name}-{param_name}.csv'), index=False,
+                          float_format='%.4f')
+            logger.to_latex(os.path.join(latex_dir, f'{name}-{param_name}.csv'), index=False,
+                            float_format='%.4f')
     print()
 
 
--- src/experiments/plot_hyperparameters.py
+++ src/experiments/plot_hyperparameters.py
@@ -12,25 +12,27 @@ args = argument_parser.parse_args()
 
 def plot():
     dir = args.csvdir
-    y_axis = 'Fitness function value'
+    y_axis = 'Fitness function value $\\times$ epoch number'
 
     for f in glob.glob(osp.join(dir, '*.csv')):
         file_name = osp.basename(f)[:-4] + '.png'
         dataframe = pd.read_csv(f)
         output_file = osp.join(osp.dirname(dir), 'plots', file_name)
-        param_name = dataframe['param name'].unique()[0]
-        param_name = ' '.join(x.capitalize() for x in param_name.split('_'))
-        param_values = dataframe['param value']
-        best = dataframe['best']
-        worst = dataframe['worst']
-        avg = dataframe['avg']
+        param_name = dataframe['param_name'].unique()[0]
+        param_name = ' '.join(x for x in param_name.split('_')).capitalize()
+        param_values = dataframe['param_value']
+        dim2 = dataframe['dimension2_mean']
+        dim10 = dataframe['dimension10_mean']
+
+        dim2_std = dataframe['dimension2_std']
+        dim10_std = dataframe['dimension10_std']
 
         plt.figure()
         plt.ylabel(y_axis)
         plt.xlabel(param_name)
-        plt.plot(param_values, best, 'o--', label='Best')
-        plt.plot(param_values, worst, 'o--', label='Worst')
-        plt.plot(param_values, avg, 'o--', label='Average')
+        plt.errorbar(param_values, dim2, fmt='o--', yerr=dim2_std, label="2 dimensions")
+        plt.errorbar(param_values, dim10, fmt='o--', yerr=dim10_std, label="10 dimensions")
+
         plt.legend(loc='best')
 
         plt.savefig(output_file)
